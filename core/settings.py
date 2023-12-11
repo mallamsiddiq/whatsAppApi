284186f40ed01b1e1d0537aaf3c7b9a6caa1dd15
akinyemi sodiq <nutfa>
2023-12-11 16:41:02 +0100

    csrf origins

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG=config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', )
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", cast=Csv(), default = ['https://*.127.0.0.1'])
 CORS_ALLOW_CREDENTIALS = True
