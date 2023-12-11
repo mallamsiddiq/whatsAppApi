e6c1d723f7b33391cede6c8e4facfc4cb832a929
akinyemi sodiq <nutfa>
2023-12-11 06:54:28 +0100

    module settings  - loadining

--- core/asgi.py
+++ core/asgi.py
@@ -1,15 +1,20 @@
 # mysite/asgi.py
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from utils.middleware.SocketTokenAuth import TokenAuthMiddlewareStack
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
+
+# import django
+# django.setup()
+
 django_asgi_app = get_asgi_application()
 
 import chat.routing
