fee607ba89995b8977e57293b46a2959d65eb280
akinyemi sodiq <nutfa>
2023-12-11 06:59:56 +0100

    module settings config

--- core/asgi.py
+++ core/asgi.py
@@ -1,24 +1,25 @@
 # mysite/asgi.py
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from utils.middleware.SocketTokenAuth import TokenAuthMiddlewareStack
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 
+
 # import django
 # django.setup()
+
 
 django_asgi_app = get_asgi_application()
 
 import chat.routing
 from chat.routing import websocket_urlpatterns
+from utils.middleware.SocketTokenAuth import TokenAuthMiddlewareStack
 
 application = ProtocolTypeRouter(
     {
